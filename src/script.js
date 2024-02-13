771cc8b4d36de065eaafad97fc5af48b2c3f3622
kasianiemiec <kasia.a.niemiec@gmail.com>
2024-02-13 22:15:32 +0200

    added search engine

--- src/script.js
+++ src/script.js
@@ -1,1 +1,8 @@
-alert("Hello");
+function search(event) {
+  event.preventDefault();
+  let city = document.querySelector("#city");
+  let h1 = document.querySelector("h1");
+  h1.innerHTML = `${city.value}`;
+}
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", search);
