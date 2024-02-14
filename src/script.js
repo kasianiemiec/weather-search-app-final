2efea6447864a3b99e39125f1135f3bbc19f3c8e
kasianiemiec <kasia.a.niemiec@gmail.com>
2024-02-14 19:58:51 +0200

    integrated the API and added the weather search

--- src/script.js
+++ src/script.js
@@ -19,12 +19,23 @@ if (minutes < 10 || hours < 10) {
   let date = document.querySelector("#current-date");
   date.innerHTML = `${day} ${hours}:${minutes}`;
 }
-
+function displayTemperature(response) {
+  let temperature = Math.round(response.data.temperature.current);
+  let currentTemp = document.querySelector("#temp");
+  currentTemp.innerHTML = temperature;
+}
 function search(event) {
   event.preventDefault();
-  let city = document.querySelector("#city");
+  let searchInput = document.querySelector("#city");
+  let city = searchInput.value;
+  let apiKey = "98bc9f158331o422ta5db05dbd90ba36";
+  let units = "metric";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=${units}`;
+
+  axios.get(apiUrl).then(displayTemperature);
+
   let h1 = document.querySelector("h1");
-  h1.innerHTML = `${city.value}`;
+  h1.innerHTML = `${city}`;
 }
 let form = document.querySelector("#search-form");
 form.addEventListener("submit", search);
